d4c3230d6e8f5905dbd8a4a2b6c85b7038d41193
Mihai <mihai.banulescu@gmail.com>
2016-08-13 19:03:11 -0700

    update jersey, add registration link to menu

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -20,7 +20,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+        format.html { redirect_to registration_path, notice: 'Player was successfully created.' }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new }
