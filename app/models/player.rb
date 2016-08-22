034ef4074491bd41cb90f699ded0e827f3e107d5
Mihai <mihai.banulescu@gmail.com>
2016-08-22 11:42:26 -0700

    make status required

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,11 @@ class Player < ApplicationRecord
   belongs_to :team, required: false
   belongs_to :user, required: false
 
+  validates :status, presence: true
+
   enum status: ['Ready to Register and Pay!', 'Very Likely to Join', 'On the Fence', 'Inactive This (EBSSL 30+) Season']
   enum age: ['30+', '28-29']
-  enum position: ['Out', '1-GK', '2-RWB', '3-LWB', '4-CB', '5-CB', '6-DM', '7-RAM', '8-DM', '9-S', '10-CAM', '11-LAM']
+  enum position: ['Out', '#1 (GK)', '#2 (RWB)', '#3 (LWB)', '#4 (CB)', '#5 (CB)', '#6 (DM)', '#7 (RAM)', '#8 (DM)', '#9 (S)', '#10 (CAM)', '#11 (LAM)']
 
   scope :inactive, -> { where(status: 3) }
   scope :active, -> { where('status <> 3').where(approved: true) }
