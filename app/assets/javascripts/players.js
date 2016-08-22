560d07f0e4deaadb3bdc9221232b7224a9c1d105
Mihai <mihai.banulescu@gmail.com>
2016-08-22 13:08:11 -0700

    ...

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
       columnDefs: [
         { type: 'num' }
       ],
-      order: [0, 'asc'],
+      order: [1, 'asc'],
       // pageLength: -1,
       // lengthMenu: [ [10, 15, 20, 25, -1], [10, 15, 20, 25, "All"] ],
       // language: { lengthMenu: "Display _MENU_ Badges" }
